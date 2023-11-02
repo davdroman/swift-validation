c280d83a403f22bdbae35afff3f0781437fdad29
David Roman <2538074+davdroman@users.noreply.github.com>
2023-11-02 14:18:40 +0000

    WIP

--- Package.swift
+++ Package.swift
@@ -62,11 +62,11 @@ let package = Package(
 //#endif
 
 package.dependencies += [
-	.package(url: "https://github.com/davdroman/swift-builders", from: "0.1.0"),
+	.package(url: "https://github.com/davdroman/swift-builders", from: "0.6.0"),
 	.package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.0.0"),
-	.package(url: "https://github.com/adam-zethraeus/Republished", from: "1.2.0"),
 	.package(url: "https://github.com/pointfreeco/swift-nonempty", from: "0.4.0"),
-	.package(url: "https://github.com/nalexn/ViewInspector", from: "0.1.0"),
+	.package(url: "https://github.com/adam-zethraeus/Republished", from: "1.2.0"),
+	.package(url: "https://github.com/nalexn/ViewInspector", from: "0.9.8"),
 ]
 
 //for target in package.targets where target.type != .system {
