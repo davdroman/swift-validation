9a3040cd74830bab0169b0a6b0e5cb418a555690
David Roman <2538074+davdroman@users.noreply.github.com>
2023-11-02 00:32:28 +0000

    wip

--- Tests/CoreValidationTests/ValidationBaseTests.swift
+++ Tests/CoreValidationTests/ValidationBaseTests.swift
@@ -11,16 +11,23 @@ final class ValidationBaseTests: XCTestCase {
 			if input.isBlank { "Cannot be blank" }
 		})
 		var sut = ""
-
+		XCTAssertEqual(sut, nil)
 		XCTAssertEqual($sut.errors, NonEmptyArray("Must be set", "Cannot be empty", "Cannot be blank"))
 
 		sut = ""
+		XCTAssertEqual(sut, nil)
 		XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be empty", "Cannot be blank"))
 
 		sut = " "
+		XCTAssertEqual(sut, nil)
 		XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be blank"))
 
 		sut = "  "
+		XCTAssertEqual(sut, nil)
 		XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be blank"))
+
+		sut = "Some value"
+		XCTAssertEqual(sut, "Some value")
+		XCTAssertEqual($sut.errors, nil)
 	}
 }
