2f1261b85578edd2274929e60c7274e14d6f4d9c
David Roman <2538074+davdroman@users.noreply.github.com>
2023-11-05 01:12:42 +0000

    WIP

--- Tests/CoreValidationTests/ValidationBaseTests.swift
+++ Tests/CoreValidationTests/ValidationBaseTests.swift
@@ -5,42 +5,46 @@ import XCTest
 
 @MainActor
 final class ValidationBaseTests: XCTestCase {
-	func testOptionalError() async {
-		await withMainSerialExecutor {
-			@ValidationBase<String, String?>({ $input in
-				if $input.isUnset { nil }
-				if input.isEmpty { "Cannot be empty" }
-				if input.isBlank { "Cannot be blank" }
-			})
-			var sut: String? = ""
-			await Task.yield()
-			XCTAssertEqual(sut, nil)
-			XCTAssertEqual($sut.errors, NonEmptyArray(nil, "Cannot be empty", "Cannot be blank"))
-
-			sut = ""
-			await Task.yield()
-			XCTAssertEqual(sut, nil)
-			XCTAssertEqual($sut.errors, NonEmptyArray(nil, "Cannot be empty", "Cannot be blank"))
-
-			sut = " "
-			await Task.yield()
-			XCTAssertEqual(sut, nil)
-			XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be blank"))
-
-			sut = ""
-			await Task.yield()
-			XCTAssertEqual(sut, nil)
-			XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be empty", "Cannot be blank"))
-
-			sut = " "
-			await Task.yield()
-			XCTAssertEqual(sut, nil)
-			XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be blank"))
-
-			sut = " S"
-			await Task.yield()
-			XCTAssertEqual(sut, " S")
-			XCTAssertEqual($sut.errors, nil)
+	override func invokeTest() {
+		withMainSerialExecutor {
+			super.invokeTest()
 		}
+	}
+
+	func testOptionalError() async {
+		@ValidationBase<String, String?>({ $input in
+			if $input.isUnset { nil }
+			if input.isEmpty { "Cannot be empty" }
+			if input.isBlank { "Cannot be blank" }
+		})
+		var sut: String? = ""
+		await Task.yield()
+		XCTAssertEqual(sut, nil)
+		XCTAssertEqual($sut.errors, NonEmptyArray(nil, "Cannot be empty", "Cannot be blank"))
+
+		sut = ""
+		await Task.yield()
+		XCTAssertEqual(sut, nil)
+		XCTAssertEqual($sut.errors, NonEmptyArray(nil, "Cannot be empty", "Cannot be blank"))
+
+		sut = " "
+		await Task.yield()
+		XCTAssertEqual(sut, nil)
+		XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be blank"))
+
+		sut = ""
+		await Task.yield()
+		XCTAssertEqual(sut, nil)
+		XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be empty", "Cannot be blank"))
+
+		sut = " "
+		await Task.yield()
+		XCTAssertEqual(sut, nil)
+		XCTAssertEqual($sut.errors, NonEmptyArray("Cannot be blank"))
+
+		sut = " S"
+		await Task.yield()
+		XCTAssertEqual(sut, " S")
+		XCTAssertEqual($sut.errors, nil)
 	}
 }
