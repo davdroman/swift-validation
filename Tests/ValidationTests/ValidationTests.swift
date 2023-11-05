a2fcaa69bfdd4ccf2b06110b59945d4b3f2d3fa8
David Roman <2538074+davdroman@users.noreply.github.com>
2023-11-05 01:09:25 +0000

    WIP

--- Tests/ValidationTests/ValidationTests.swift
+++ Tests/ValidationTests/ValidationTests.swift
@@ -18,26 +18,12 @@
 //		})
 //		var input: String? = nil
 //
-////		@Validation(mode: .live(delay: .seconds(0.25)) | .late, { ... ❌ - too ambiguous ("live" being vernacular in TCA, "late" is too much of an obscure UX term)
-////		@Validation(mode: .asYouType(delay: .seconds(0.25)) | .onSubmit, { ... ❌ - should be agnostic (e.g. sliders)
-////		@Validation(mode: .automatic(delay: .seconds(0.25)) | .manual, { ... ✅ best one so far
-////
-////		@Validation(mode: .automatic(delay: .seconds(0.25)) | .manual, { past, new, error in // ❌ "past" could mean history, but it's still a long shot
-////		@Validation(mode: .automatic(delay: .seconds(0.25)) | .manual, { pastValues, newValue, error in // ❌ "past" is longer than "old"
-////		@Validation(mode: .automatic(delay: .seconds(0.25)) | .manual, { oldValues, newValue, error in // ✅ most similar to SwiftUI APIs (e.g. .onChange)
-////			guard let new else { error("Name cannot be nil"); return }
-////			if new.isBlank { error("Name cannot be blank") }
-////			if new.count < 2 { error("Name cannot be shorter than 2 characters") }
-////			if new.rangeOfCharacter(from: .symbols) != nil { error("Name cannot contain special characters or symbols") }
-////		})
-////		var input: String? = nil
+//		$input.validate()
 //
-////		$input.validate()
-//
-////		@TaskLocal enclosingInstance: Any?
-////		@Get(\Self.name) var name // reads from $enclosingInstance
-////		@Property(\Self.name) var name // reads from $enclosingInstance
-////		@Get(\Self.name) var name // reads from $enclosingInstance
+//		@TaskLocal enclosingInstance: Any?
+//		@Get(\Self.name) var name // reads from $enclosingInstance
+//		@Property(\Self.name) var name // reads from $enclosingInstance
+//		@Get(\Self.name) var name // reads from $enclosingInstance
 //
 //		XCTAssertNil(input)
 //		XCTAssertEqual($input.errors, NonEmptyArray("Input cannot be nil"))
