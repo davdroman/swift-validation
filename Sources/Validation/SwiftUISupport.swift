b56b1601abb667d1dc947b1767418a95df65e3d1
David Roman <2538074+davdroman@users.noreply.github.com>
2025-12-08 11:27:07 +0000

    WIP [ci skip]

--- Sources/Validation/SwiftUISupport.swift
+++ Sources/Validation/SwiftUISupport.swift
@@ -3,7 +3,30 @@ import SwiftUI
 
 public typealias LocalizedValidation<Value> = Validation<Value, LocalizedStringKey?>
 
-#Preview {
+@MainActor
+extension Binding {
+	public init<Error>(
+		_ validation: Validation<Value, Error>
+	) {
+		self.init(
+			get: { validation.rawValue },
+			set: { validation.wrappedValue = $0 }
+		)
+	}
+
+	public init<Error>(
+		_ validation: Validation<Value?, Error>,
+		default defaultValue: Value
+	) {
+		self.init(
+			get: { validation.rawValue ?? defaultValue },
+			set: { validation.wrappedValue = $0 }
+		)
+	}
+}
+
+#if DEBUG
+#Preview("Bare") {
 	@Previewable @Validation({ $name in
 		let _ = await {
 			do { try await Task.sleep(nanoseconds: NSEC_PER_SEC/2) }
@@ -40,4 +63,49 @@ public typealias LocalizedValidation<Value> = Validation<Value, LocalizedStringK
 	}
 	.padding()
 }
+
+@Observable
+final class Inputs {
+	@ObservationIgnored
+	@Validation<String?, String>({ $input in
+		switch input {
+		case nil: "Cannot be nil"
+		case let input?:
+			if input.isEmpty { "Cannot be empty" }
+			if input.isBlank { "Cannot be blank" }
+		}
+	})
+	var inputA = nil
+
+	@ObservationIgnored
+	@Validation<String?, String>({ $input in
+		switch input {
+		case nil: "Cannot be nil"
+		case let input?:
+			if input.isEmpty { "Cannot be empty" }
+			if input.isBlank { "Cannot be blank" }
+		}
+	})
+	var inputB = nil
+}
+
+#Preview("@State") {
+	@Previewable @State var inputs = Inputs()
+
+	VStack(alignment: .leading) {
+		TextField(
+			"Name",
+			text: Binding(inputs.$inputA, default: "")
+		)
+		.textFieldStyle(.roundedBorder)
+
+		if let error = inputs.$inputA.errors?.first {
+			Text(error)
+				.foregroundColor(.red)
+				.font(.footnote)
+		}
+	}
+	.padding()
+}
+#endif
 #endif
