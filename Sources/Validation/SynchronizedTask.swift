b23eeee247b3003443e276540806431984325e37
David Roman <2538074+davdroman@users.noreply.github.com>
2025-12-08 11:53:27 +0000

    WIP [ci skip]

--- Sources/Validation/SynchronizedTask.swift
+++ Sources/Validation/SynchronizedTask.swift
@@ -1,155 +1,155 @@
-//import ConcurrencyExtras
-//import Foundation
-//
-//protocol Cancellable {
-//	func cancel()
-//}
-//
-//extension Task: Cancellable {}
-//extension SynchronizedTask: Cancellable {}
-//
-//@MainActor
-//struct SynchronizedTask {
-//	typealias Operation = (_ synchronize: @Sendable () async throws -> Void) async throws -> Void
-//
-//	private let path: SynchronizedTaskPool.Path
-//	private let task: Task<Void, Never>
-//
-//	@discardableResult
-//	init(
-//		id: some Hashable,
-//		priority: TaskPriority? = nil,
-//		operation: @escaping Operation,
-//		onCancel: (@Sendable () async -> Void)? = nil // TODO: is this needed for handling cancellation of manual group validation?
-//	) {
-//		self.path = SynchronizedTaskPool.shared.prepare(id: id)
-//		self.task = Task { [path] in
-//			do {
-//				try SynchronizedTaskPool.shared.start(path: path)
-//				try await operation({ try await SynchronizedTaskPool.shared.finish(path: path) })
-//			} catch is CancellationError {
-//				await onCancel?()
-//			} catch {}
-//		}
-//	}
-//
-//	func cancel() {
-//		SynchronizedTaskPool.shared.cancel(path: path)
-//		task.cancel()
-//	}
-//}
-//
-//@MainActor
-//fileprivate final class SynchronizedTaskPool {
-//	static let shared = SynchronizedTaskPool()
-//
-//	struct Path: Hashable, Sendable {
-//		let id: AnyHashable
-//		let seed: UUID
-//	}
-//
-//	struct TasksState {
-//		enum Status {
-//			case idle
-//			case inProgress
-//			case cancelled
-//		}
-//
-//		var count: Int = 0
-//		var status: Status = .idle
-//
-//		mutating func prepare() {
-//			switch status {
-//			case .idle:
-//				count += 1
-//			case .inProgress, .cancelled:
-//				count = 1
-//				status = .idle
-//			}
-//		}
-//
-//		mutating func start() throws {
-//			switch status {
-//			case .idle:
-//				status = .inProgress
-//			case .inProgress:
-//				break
-//			case .cancelled:
-//				throw CancellationError()
-//			}
-//		}
-//
-//		mutating func finish() throws {
-//			switch status {
-//			case .idle:
-//				assertionFailure("Internal state inconsistency: cannot synchronize idle tasks")
-//			case .inProgress:
-//				count -= 1
-//			case .cancelled:
-//				throw CancellationError()
-//			}
-//		}
-//
-//		mutating func cancel() {
-//			switch status {
-//			case .idle, .inProgress:
-//				status = .cancelled
-//			case .cancelled:
-//				break
-//			}
-//		}
-//	}
-//
-//	var seeds: LockIsolated<[AnyHashable: UUID]> = .init([:])
-//	var tasks: LockIsolated<[Path: TasksState]> = .init([:])
-//
-//	func prepare(id: some Hashable) -> Path {
-//		let path = seeds.withValue { seeds in
-//			let seed = seeds[id] ?? UUID()
-//			if seeds[id] == nil {
-//				seeds[id] = seed
-//			}
-//			return Path(id: id, seed: seed)
-//		}
-//
-//		tasks.withValue {
-//			var state = $0[path] ?? .init()
-//			state.prepare()
-//			$0[path] = state
-//		}
-//
-//		return path
-//	}
-//
-//	func start(path: Path) throws {
-//		seeds.withValue {
-//			$0[path.id] = nil
-//		}
-//		try tasks.withValue {
-//			try $0[path]?.start()
-//		}
-//	}
-//
-//	func finish(path: Path) async throws {
-//		try tasks.withValue {
-//			try $0[path]?.finish()
-//		}
-//
-//		while let state = tasks.value[path], state.status == .inProgress, state.count > 0 {
-//			await Task.yield()
-//		}
-//
-//		if tasks.value[path]?.status != .inProgress {
-//			throw CancellationError()
-//		}
-//	}
-//
-//	func cancel(path: Path) {
-//		seeds.withValue {
-//			$0[path.id] = nil
-//		}
-//		tasks.withValue {
-//			$0[path]?.cancel()
-//		}
-//	}
-//}
+import ConcurrencyExtras
+import Foundation
+
+protocol Cancellable {
+	func cancel()
+}
+
+extension Task: Cancellable {}
+extension SynchronizedTask: Cancellable {}
+
+@MainActor
+struct SynchronizedTask {
+	typealias Operation = (_ synchronize: @Sendable () async throws -> Void) async throws -> Void
+
+	private let path: SynchronizedTaskPool.Path
+	private let task: Task<Void, Never>
+
+	@discardableResult
+	init(
+		id: some Hashable,
+		priority: TaskPriority? = nil,
+		operation: @escaping Operation,
+		onCancel: (@Sendable () async -> Void)? = nil // TODO: is this needed for handling cancellation of manual group validation?
+	) {
+		self.path = SynchronizedTaskPool.shared.prepare(id: id)
+		self.task = Task { [path] in
+			do {
+				try SynchronizedTaskPool.shared.start(path: path)
+				try await operation({ try await SynchronizedTaskPool.shared.finish(path: path) })
+			} catch is CancellationError {
+				await onCancel?()
+			} catch {}
+		}
+	}
+
+	func cancel() {
+		SynchronizedTaskPool.shared.cancel(path: path)
+		task.cancel()
+	}
+}
+
+@MainActor
+fileprivate final class SynchronizedTaskPool {
+	static let shared = SynchronizedTaskPool()
+
+	struct Path: Hashable, Sendable {
+		let id: AnyHashable
+		let seed: UUID
+	}
+
+	struct TasksState {
+		enum Status {
+			case idle
+			case inProgress
+			case cancelled
+		}
+
+		var count: Int = 0
+		var status: Status = .idle
+
+		mutating func prepare() {
+			switch status {
+			case .idle:
+				count += 1
+			case .inProgress, .cancelled:
+				count = 1
+				status = .idle
+			}
+		}
+
+		mutating func start() throws {
+			switch status {
+			case .idle:
+				status = .inProgress
+			case .inProgress:
+				break
+			case .cancelled:
+				throw CancellationError()
+			}
+		}
+
+		mutating func finish() throws {
+			switch status {
+			case .idle:
+				assertionFailure("Internal state inconsistency: cannot synchronize idle tasks")
+			case .inProgress:
+				count -= 1
+			case .cancelled:
+				throw CancellationError()
+			}
+		}
+
+		mutating func cancel() {
+			switch status {
+			case .idle, .inProgress:
+				status = .cancelled
+			case .cancelled:
+				break
+			}
+		}
+	}
+
+	var seeds: LockIsolated<[AnyHashable: UUID]> = .init([:])
+	var tasks: LockIsolated<[Path: TasksState]> = .init([:])
+
+	func prepare(id: some Hashable) -> Path {
+		let path = seeds.withValue { seeds in
+			let seed = seeds[id] ?? UUID()
+			if seeds[id] == nil {
+				seeds[id] = seed
+			}
+			return Path(id: id, seed: seed)
+		}
+
+		tasks.withValue {
+			var state = $0[path] ?? .init()
+			state.prepare()
+			$0[path] = state
+		}
+
+		return path
+	}
+
+	func start(path: Path) throws {
+		seeds.withValue {
+			$0[path.id] = nil
+		}
+		try tasks.withValue {
+			try $0[path]?.start()
+		}
+	}
+
+	func finish(path: Path) async throws {
+		try tasks.withValue {
+			try $0[path]?.finish()
+		}
+
+		while let state = tasks.value[path], state.status == .inProgress, state.count > 0 {
+			await Task.yield()
+		}
+
+		if tasks.value[path]?.status != .inProgress {
+			throw CancellationError()
+		}
+	}
+
+	func cancel(path: Path) {
+		seeds.withValue {
+			$0[path.id] = nil
+		}
+		tasks.withValue {
+			$0[path]?.cancel()
+		}
+	}
+}
--- Tests/ValidationTests/SynchronizedTaskTests.swift
+++ Tests/ValidationTests/SynchronizedTaskTests.swift
@@ -1,162 +1,162 @@
-//@testable import Validation
-//import Dependencies
-//import XCTest
-//
-//@MainActor
-//final class SynchronizedTaskTests: XCTestCase {
-//	@MainActor
-//	final class Doer {
-//		private var task: SynchronizedTask? = nil
-//
-//		func `do`(
-//			for duration: Duration,
-//			id: some Hashable,
-//			didFinish: ActorIsolated<Bool>,
-//			cancellationCount: ActorIsolated<Int>? = nil
-//		) {
-//			task?.cancel()
-//			task = SynchronizedTask(id: id) { synchronize in
-//				@Dependency(\.continuousClock) var clock
-//				try await clock.sleep(for: duration) // simulates work happening
-//				try await synchronize()
-//				await didFinish.setValue(true)
-//			} onCancel: {
-//				await cancellationCount?.withValue { $0 += 1 }
-//			}
-//		}
-//	}
-//
-//	let doer1 = Doer()
-//	let doer2 = Doer()
-//	let doer3 = Doer()
-//
-//	let doer1DidFinish = ActorIsolated<Bool>(false)
-//	let doer2DidFinish = ActorIsolated<Bool>(false)
-//	let doer3DidFinish = ActorIsolated<Bool>(false)
-//
-//	let doer1CancellationCount = ActorIsolated<Int>(0)
-//	let doer2CancellationCount = ActorIsolated<Int>(0)
-//	let doer3CancellationCount = ActorIsolated<Int>(0)
-//
-//	var allDidFinishValues: [Bool] {
-//		get async {
-//			await [
-//				doer1DidFinish.value,
-//				doer2DidFinish.value,
-//				doer3DidFinish.value,
-//			]
-//		}
-//	}
-//
-//	var allCancellationCountValues: [Int] {
-//		get async {
-//			await [
-//				doer1CancellationCount.value,
-//				doer2CancellationCount.value,
-//				doer3CancellationCount.value,
-//			]
-//		}
-//	}
-//
-//	let clock = TestClock()
-//
-//	// MARK: Tests
-//
-//	override func invokeTest() {
-//		withMainSerialExecutor {
-//			super.invokeTest()
-//		}
-//	}
-//
-//	func testHappyPath() async {
-//		withDependencies {
-//			$0.continuousClock = clock
-//		} operation: {
-//			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish)
-//			doer2.do(for: .seconds(2), id: 1, didFinish: doer2DidFinish)
-//			doer3.do(for: .seconds(3), id: 1, didFinish: doer3DidFinish)
-//		}
-//
-//		var didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { !$0 })
-//
-//		await clock.advance(by: .seconds(1))
-//
-//		didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { !$0 })
-//
-//		await clock.advance(by: .seconds(1))
-//
-//		didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { !$0 })
-//
-//		await clock.advance(by: .seconds(1))
-//
-//		didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { $0 })
-//	}
-//
-//	func testAsyncCancellation() async throws {
-//		withDependencies {
-//			$0.continuousClock = clock
-//		} operation: {
-//			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish, cancellationCount: doer1CancellationCount)
-//			doer2.do(for: .seconds(2), id: 1, didFinish: doer2DidFinish, cancellationCount: doer2CancellationCount)
-//			doer3.do(for: .seconds(3), id: 1, didFinish: doer3DidFinish, cancellationCount: doer3CancellationCount)
-//		}
-//
-//		var didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { !$0 })
-//
-//		await clock.advance(by: .seconds(1))
-//
-//		didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { !$0 })
-//
-//		await clock.advance(by: .seconds(1))
-//
-//		didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { !$0 })
-//
-//		withDependencies {
-//			$0.continuousClock = clock
-//		} operation: {
-//			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish, cancellationCount: doer1CancellationCount)
-//		}
-//
-//		await clock.run()
-//
-//		didFinishValues = await allDidFinishValues
-//		XCTAssertEqual(didFinishValues, [true, false, false])
-//
-//		let cancellationCountValues = await allCancellationCountValues
-//		XCTAssertEqual(cancellationCountValues, [1, 1, 1])
-//	}
-//
-//	func testImmediateCancellation() async throws {
-//		withDependencies {
-//			$0.continuousClock = clock
-//		} operation: {
-//			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish, cancellationCount: doer1CancellationCount)
-//			doer2.do(for: .seconds(2), id: 1, didFinish: doer2DidFinish, cancellationCount: doer2CancellationCount)
-//			doer3.do(for: .seconds(3), id: 1, didFinish: doer3DidFinish, cancellationCount: doer3CancellationCount)
-//		}
-//
-//		withDependencies {
-//			$0.continuousClock = clock
-//		} operation: {
-//			doer2.do(for: .seconds(1), id: 1, didFinish: doer2DidFinish, cancellationCount: doer1CancellationCount)
-//		}
-//
-//		var didFinishValues = await allDidFinishValues
-//		XCTAssert(didFinishValues.allSatisfy { !$0 })
-//
-//		await clock.run()
-//
-//		didFinishValues = await allDidFinishValues
-//		XCTAssertEqual(didFinishValues, [false, true, false])
-//
-//		let cancellationCountValues = await allCancellationCountValues
-//		XCTAssertEqual(cancellationCountValues, [1, 1, 1])
-//	}
-//}
+@testable import Validation
+import Dependencies
+import XCTest
+
+@MainActor
+final class SynchronizedTaskTests: XCTestCase {
+	@MainActor
+	final class Doer {
+		private var task: SynchronizedTask? = nil
+
+		func `do`(
+			for duration: Duration,
+			id: some Hashable,
+			didFinish: ActorIsolated<Bool>,
+			cancellationCount: ActorIsolated<Int>? = nil
+		) {
+			task?.cancel()
+			task = SynchronizedTask(id: id) { synchronize in
+				@Dependency(\.continuousClock) var clock
+				try await clock.sleep(for: duration) // simulates work happening
+				try await synchronize()
+				await didFinish.setValue(true)
+			} onCancel: {
+				await cancellationCount?.withValue { $0 += 1 }
+			}
+		}
+	}
+
+	let doer1 = Doer()
+	let doer2 = Doer()
+	let doer3 = Doer()
+
+	let doer1DidFinish = ActorIsolated<Bool>(false)
+	let doer2DidFinish = ActorIsolated<Bool>(false)
+	let doer3DidFinish = ActorIsolated<Bool>(false)
+
+	let doer1CancellationCount = ActorIsolated<Int>(0)
+	let doer2CancellationCount = ActorIsolated<Int>(0)
+	let doer3CancellationCount = ActorIsolated<Int>(0)
+
+	var allDidFinishValues: [Bool] {
+		get async {
+			await [
+				doer1DidFinish.value,
+				doer2DidFinish.value,
+				doer3DidFinish.value,
+			]
+		}
+	}
+
+	var allCancellationCountValues: [Int] {
+		get async {
+			await [
+				doer1CancellationCount.value,
+				doer2CancellationCount.value,
+				doer3CancellationCount.value,
+			]
+		}
+	}
+
+	let clock = TestClock()
+
+	// MARK: Tests
+
+	override func invokeTest() {
+		withMainSerialExecutor {
+			super.invokeTest()
+		}
+	}
+
+	func testHappyPath() async {
+		withDependencies {
+			$0.continuousClock = clock
+		} operation: {
+			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish)
+			doer2.do(for: .seconds(2), id: 1, didFinish: doer2DidFinish)
+			doer3.do(for: .seconds(3), id: 1, didFinish: doer3DidFinish)
+		}
+
+		var didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { !$0 })
+
+		await clock.advance(by: .seconds(1))
+
+		didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { !$0 })
+
+		await clock.advance(by: .seconds(1))
+
+		didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { !$0 })
+
+		await clock.advance(by: .seconds(1))
+
+		didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { $0 })
+	}
+
+	func testAsyncCancellation() async throws {
+		withDependencies {
+			$0.continuousClock = clock
+		} operation: {
+			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish, cancellationCount: doer1CancellationCount)
+			doer2.do(for: .seconds(2), id: 1, didFinish: doer2DidFinish, cancellationCount: doer2CancellationCount)
+			doer3.do(for: .seconds(3), id: 1, didFinish: doer3DidFinish, cancellationCount: doer3CancellationCount)
+		}
+
+		var didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { !$0 })
+
+		await clock.advance(by: .seconds(1))
+
+		didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { !$0 })
+
+		await clock.advance(by: .seconds(1))
+
+		didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { !$0 })
+
+		withDependencies {
+			$0.continuousClock = clock
+		} operation: {
+			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish, cancellationCount: doer1CancellationCount)
+		}
+
+		await clock.run()
+
+		didFinishValues = await allDidFinishValues
+		XCTAssertEqual(didFinishValues, [true, false, false])
+
+		let cancellationCountValues = await allCancellationCountValues
+		XCTAssertEqual(cancellationCountValues, [1, 1, 1])
+	}
+
+	func testImmediateCancellation() async throws {
+		withDependencies {
+			$0.continuousClock = clock
+		} operation: {
+			doer1.do(for: .seconds(1), id: 1, didFinish: doer1DidFinish, cancellationCount: doer1CancellationCount)
+			doer2.do(for: .seconds(2), id: 1, didFinish: doer2DidFinish, cancellationCount: doer2CancellationCount)
+			doer3.do(for: .seconds(3), id: 1, didFinish: doer3DidFinish, cancellationCount: doer3CancellationCount)
+		}
+
+		withDependencies {
+			$0.continuousClock = clock
+		} operation: {
+			doer2.do(for: .seconds(1), id: 1, didFinish: doer2DidFinish, cancellationCount: doer1CancellationCount)
+		}
+
+		var didFinishValues = await allDidFinishValues
+		XCTAssert(didFinishValues.allSatisfy { !$0 })
+
+		await clock.run()
+
+		didFinishValues = await allDidFinishValues
+		XCTAssertEqual(didFinishValues, [false, true, false])
+
+		let cancellationCountValues = await allCancellationCountValues
+		XCTAssertEqual(cancellationCountValues, [1, 1, 1])
+	}
+}
